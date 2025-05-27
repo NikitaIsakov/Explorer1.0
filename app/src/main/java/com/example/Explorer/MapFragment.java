5f50c1833ea8c903d520015da9223d30a9f8d2ff
Nikita Isakov <nikita.isakov@ccsteam.ru>
2025-05-27 21:24:31 +0700

    added ya map

--- app/src/main/java/com/example/Explorer/MapFragment.java
+++ app/src/main/java/com/example/Explorer/MapFragment.java
@@ -5,14 +5,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
 
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.Animation;
+
 public class MapFragment extends Fragment {
+    private MapView mapView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("101b87ca-0d24-48ad-b7f8-a9a932102329");
+        MapKitFactory.initialize(requireContext());
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = view.findViewById(R.id.mapView);
+
+        mapView.getMap().move(
+                new CameraPosition(
+                        new Point(55.751574, 37.573856),
+                        10.0f,
+                        0.0f,
+                        0.0f
+                ),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null
+        );
+
+        return view;
+    }
+
+    @Override
+    public void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
     }
 }
